926be7c3a0ad2d3f428fc170b1f28d0c7df98564
Olof Buhlin <tidus_242@hotmail.com>
2016-03-30 12:34:16 +0200

    dimond shape added

--- inlamning1/MainWindow.xaml.cs
+++ inlamning1/MainWindow.xaml.cs
@@ -37,5 +37,56 @@ namespace inlamning1
                 this.Close();
             }
         }
+
+        private void btn_ellipse_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an Ellipse
+            Ellipse ellipse = new Ellipse();
+            ellipse.Height = 75;
+            ellipse.Width = 150;
+
+            // Create a blue and a black Brush
+            SolidColorBrush fillColour = new SolidColorBrush();
+            fillColour.Color = Colors.Orange;
+            SolidColorBrush edgeColour = new SolidColorBrush();
+            edgeColour.Color = Colors.Black;
+
+            // Set Ellipse's width and color
+            ellipse.StrokeThickness = 1;
+            ellipse.Stroke = edgeColour;
+            // Fill rectangle with blue color
+            ellipse.Fill = fillColour;
+
+            // Add Ellipse to the Grid.
+            Drawarea.Children.Add(ellipse);
+        }
+
+        private void btn_diamond_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an Ellipse
+            Rectangle diamond = new Rectangle();
+            diamond.Height = 75;
+            diamond.Width = 75;
+            int angle = 45;
+            diamond.RenderTransform = new RotateTransform(angle, diamond.Height /2, diamond.Width /2);
+
+
+            // Create a blue and a black Brush
+            SolidColorBrush fillColour = new SolidColorBrush();
+            fillColour.Color = Colors.Orange;
+            SolidColorBrush edgeColour = new SolidColorBrush();
+            edgeColour.Color = Colors.Black;
+
+            // Set Ellipse's width and color
+            diamond.StrokeThickness = 1;
+            diamond.Stroke = edgeColour;
+            // Fill rectangle with blue color
+            diamond.Fill = fillColour;
+
+            // Add Ellipse to the Grid.
+            Drawarea.Children.Add(diamond);
+            
+        }
     }
+    
 }
